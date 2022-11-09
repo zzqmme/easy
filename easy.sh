f3cebefe2164e125db1d6f4f01b92b500d80c4ab
zzqmme <43514109+zzqmme@users.noreply.github.com>
2022-11-09 16:14:47 +0300

    Update easy.sh

--- easy.sh
+++ easy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+if [ "$#" -ne "1" ]; then
+	echo "ERROR: Invalid number of arguments"
+	exit 1
+fi
+
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+  echo "ERROR: Not a number"
+  exit 1
+fi
 echo $(($1 - 1))
 exit 0
